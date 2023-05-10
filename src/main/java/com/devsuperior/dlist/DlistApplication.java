63bf5d54829fb6da3a0d3f50c10f43bfb344517b
Wellington Felix <wffelixy@gmail.com>
2023-05-10 15:35:03 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dlist/DlistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dlist/DlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
