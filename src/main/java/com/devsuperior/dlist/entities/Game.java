ab2e433061072fa2efde4958e6ee8dce8eab1fde
Wellington Felix <wffelixy@gmail.com>
2023-05-11 00:47:21 -0300

    Ajuste na variavel platforms

--- src/main/java/com/devsuperior/dlist/entities/Game.java
+++ src/main/java/com/devsuperior/dlist/entities/Game.java
@@ -1,5 +1,4 @@
 package com.devsuperior.dlist.entities;
-
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -27,19 +26,15 @@ public class Game {
 	
 	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	
+
 	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game() {
-		
-	}
-
-	
-
+    public Game() {
+    }
+    
 	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
@@ -48,118 +43,80 @@ public class Game {
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.longDescription = longDescription;		
 	}
-
-
 
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public String getTitle() {
 		return title;
 	}
 
-
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-
 
 	public Integer getYear() {
 		return year;
 	}
 
-
-
 	public void setYear(Integer year) {
 		this.year = year;
 	}
-
-
 
 	public String getGenre() {
 		return genre;
 	}
 
-
-
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
 
-
-
-	public String getPlatform() {
+	public String getPlatforms() {
 		return platforms;
 	}
 
-
-
-	public void setPlatform(String platforms) {
+	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
 	}
-
-
 
 	public Double getScore() {
 		return score;
 	}
 
-
-
 	public void setScore(Double score) {
 		this.score = score;
 	}
-
-
 
 	public String getImgUrl() {
 		return imgUrl;
 	}
 
-
-
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-
-
 
 	public String getShortDescription() {
 		return shortDescription;
 	}
 
-
-
 	public void setShortDescription(String shortDescription) {
 		this.shortDescription = shortDescription;
 	}
-
-
 
 	public String getLongDescription() {
 		return longDescription;
 	}
 
-
-
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	}
-
-
 
 	@Override
 	public int hashCode() {
@@ -177,7 +134,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
