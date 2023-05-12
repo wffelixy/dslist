6e8760d1f8ccee48fcbfd70507af25a2be8d1d1e
Wellington Felix <wffelixy@gmail.com>
2023-05-12 10:58:10 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dlist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dlist/dto/GameMinDTO.java
@@ -23,7 +23,7 @@ public class GameMinDTO {
 		super();
 		id = projection.getId();
 		title = projection.getTitle();
-		year = projection.getYear();
+		year = projection.getGameYear();
 		imgUrl = projection.getImgUrl();
 		shortDescription = projection.getShortDescription();
 	}
